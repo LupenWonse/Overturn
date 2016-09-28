e88ebe72fb21be38d514268b990695b14e4bfadf
Ahmet Gencoglu <ahmetgencoglu@gmail.com>
2016-09-27 23:45:15 -0400

    Initial commit
    Initial Commit - Menu + Level01

---
+++ Assets/scripts/PlayerController.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	public float maxSpeed = 10f;
+	Animator anim;
+	// Use this for initialization
+	void Start (){
+		anim = GetComponent<Animator>();
+	}
+
+
+
+	// Update is called once per frame
+	void Update () {
+		var move = Input.GetAxis("Horizontal");
+		if(!Universe.inverse) {
+			if(move>0) {
+				anim.SetInteger("Direction", 0);
+				GetComponent<Rigidbody2D>().velocity = new Vector2(10*move*maxSpeed, GetComponent<Rigidbody2D>().velocity.y);
+				anim.SetFloat("Speed", Mathf.Abs(move));
+			}
+			else if(move <0) {
+				anim.SetInteger("Direction", 1);
+				GetComponent<Rigidbody2D>().velocity = new Vector2(10*move*maxSpeed, GetComponent<Rigidbody2D>().velocity.y);
+				anim.SetFloat("Speed", Mathf.Abs(move));
+			}else {
+				anim.SetFloat("Speed", Mathf.Abs(0*maxSpeed));
+				GetComponent<Rigidbody2D>().velocity = new Vector2(0*move*maxSpeed, GetComponent<Rigidbody2D>().velocity.y); }
+
+		}
+
+		else{
+			if(move>0) {
+				anim.SetInteger("Direction", 1);
+				GetComponent<Rigidbody2D>().velocity = new Vector2(-10*move*maxSpeed, GetComponent<Rigidbody2D>().velocity.y);
+				anim.SetFloat("Speed", Mathf.Abs(move));
+			}
+			else if(move <0) {
+				anim.SetInteger("Direction", 0);
+				GetComponent<Rigidbody2D>().velocity = new Vector2(-10*move*maxSpeed, GetComponent<Rigidbody2D>().velocity.y);
+				anim.SetFloat("Speed", Mathf.Abs(move));
+			}else {
+				anim.SetFloat("Speed", Mathf.Abs(0*maxSpeed));
+				GetComponent<Rigidbody2D>().velocity = new Vector2(0*move*maxSpeed, GetComponent<Rigidbody2D>().velocity.y); }
+
+		}
+
+
+
+			}
+}
---
+++ Assets/scripts/Universe.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Universe : MonoBehaviour {
+	public static bool inverse;
+	public bool test;
+	public GameObject bg;
+	public GameObject bginverse;
+	SpriteRenderer bgSprite;
+	SpriteRenderer bgInverseSprite;
+
+
+	// Use this for initialization
+	void Start () {
+	Universe.inverse = false;
+	test = false;
+	Invoke("Inverse", 7);
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void testOnClick () {
+		print ("Hello");
+	}
+
+	void Inverse() {
+		Universe.inverse = !Universe.inverse;
+		float random = Random.Range(5f, 10f);
+		Invoke("Inverse", random);
+	}
+}
