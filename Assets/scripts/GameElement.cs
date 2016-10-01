51541e496740584d0483334f5a6815401d659033
Ahmet Gencoglu <ahmetgencoglu@gmail.com>
2016-10-01 00:13:46 -0400

    Fireball throwing implementation
    Implemented game objects that can throw killer objects

--- Assets/scripts/GameElement.cs
+++ Assets/scripts/GameElement.cs
@@ -7,17 +7,28 @@ public class GameElement : MonoBehaviour {
 	public bool door = false;
 	public Vector2 moving = new Vector2(0,0);
 	public float movementSpeed = 1.0f;
+	public GameObject throws;
+
 	private Vector3 initialPosition;
+
+
 
 	private int direction = 1;
 
 	void Start(){
 		initialPosition = transform.position;
+
+		if (throws != null) {
+			startThrowing ();
+		}
 	}
 
 	void FixedUpdate(){
-		if (moving.magnitude > 0) {
+		if (moving.x > 0) {
 			GetComponent<Rigidbody2D> ().velocity = new Vector2 (movementSpeed * direction, 0);
+		}
+		if (moving.y > 0) {
+			GetComponent<Rigidbody2D> ().velocity = new Vector2 (0, movementSpeed * direction);
 		}
 
 		if (this.transform.position.x - initialPosition.x > moving.x) {
@@ -25,5 +36,11 @@ public class GameElement : MonoBehaviour {
 		} else if (this.transform.position.x - initialPosition.x < -moving.x){
 			direction = 1;
 		}
+	}
+
+	void startThrowing(){
+		print ("Throwing");
+		GameObject.Instantiate (throws);
+		Invoke ("startThrowing", 4);
 	}
 }
